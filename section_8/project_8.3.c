1bfa484c71dc42ef714476ee7fd50f89d49e041b
EAksoy <aksoyemin1@gmail.com>
2024-07-10 11:18:58 +0100

    Finished project 3, and added projects 4 through 8. 8 is still WIP.

--- section_8/project_8.3.c
+++ section_8/project_8.3.c
@@ -3,6 +3,35 @@
 
 int main(){
 
-	
+    int digit;
+    long n;
+
+    while(true){
+        bool digit_seen[10] = {false};
+
+        printf("Enter a number, enter 0 to terminate program: ");
+        scanf("%ld", &n);
+        if (n == 0){
+            break;
+        }
+
+        while(n > 0){
+            digit = n % 10;
+            if(digit_seen[digit]){
+                break;
+            }
+            digit_seen[digit] = true;
+            n /= 10;
+        }
+
+        if(n > 0){
+            printf("Repeated Digit.\n");
+        }
+        else{
+            printf("No Repeated Digits.\n");
+        }
+        
+    }
+    return 0;
 
 }
